b746b4bb1c78a1c1196c245d9d5d8291559b12cd
Haikal Y Simarmata <149587402+HaikalSimarmata@users.noreply.github.com>
2025-02-06 14:16:49 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+// // function random(max){
+// //     return Math.floor(Math.random() * max);
+// // };
+
+// // console.log(random(10))
+
+
+// // function getRandom(min, max){
+// // return Math.floor(Math.random()*(max-min + 1))+min
+// // }
+
+// // console.log(getRandom(1,20))
+
+
+// // function jumlah(a,b){
+// //     hasil = a * b;
+// //     return hasil;
+// // }
+
+// // console.log(jumlah(2,4))
+
+// let arr = ["haikal", "yudistira", "simarmata"];
+// for(i=0; i < arr.length;i++){
+//     console.log('mahasiswa ke- ',i+1,':'+ arr[i]);
+// }
+
+
+// const nama = ["1","2","4","6","8"];
+
+// // console.log(nama[0])
+// // for(i=0; i < nama.length; i++){
+// //     console.log(nama.length)
+// // }
+     
+// // arr.forEach(function(e){
+// //     console.log(e)
+// // })
+
+// // arr.forEach(function(a,i){
+// //     console.log('mahasiswa ke- ',i+1 + ":", a)
+// // })
+
+// // let nama2 = nama.map(function(a){
+// //     return a * 2
+// // });
+// // console.log(nama2.join('-'))
+
+// let angka = [1,6,3,8,4,5]
+// angka.sort();
+// console.log(angka.join('-'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let klik = document.querySelector('.klik');
+
+klik.addEventListener('click', function(){
+    console.log('klik')
+})
